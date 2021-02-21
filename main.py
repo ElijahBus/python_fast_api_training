fff2eddb626a902b51b753e7f4aa26da34595acf
Elijah Busanga <elijahbus.ny@gmail.com>
2021-02-21 14:43:29 +0200

    Routing and api documentatio

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 def index():
-    return {'data': {'name': 'Elijah'}}
+    return {'data': 'blog list'}
 
-@app.get('/about')
-def about():
-    return {'data': 'About page'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'List of unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def about(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    # fetch comments of a blog
+    return {'data': {'1', '2'}}
