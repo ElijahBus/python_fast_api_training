4817cbd8a9423d80fab8eda2edfa89a03dddd8b2
Elijah Busanga <elijahbus.ny@gmail.com>
2021-02-21 15:15:20 +0200

    Query parameters

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
+
 
 app = FastAPI()
 
 
 @app.get('/')
-def index():
-    return {'data': 'blog list'}
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+
+    return {'data': f'{limit} blogs from the db'}
 
 
 @app.get('/blog/unpublished')
